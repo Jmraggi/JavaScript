4e893007c89ca4f7a3bf6684f67f6f7e1596d80f
Juan Manuel <juanmanuelraggi@gmail.com>
2022-07-07 11:55:47 -0300

    entregaStorage

--- src/js/main.js
+++ src/js/main.js
@@ -87,3 +87,117 @@ carrito.forEach(producto => {
     listaCarrito.appendChild(table)
 })}
 
+
+//--------------CALIFICACIONES-----------------
+
+class Opiniones {
+  constructor(nombre, puntaje,opinion) {
+    this.nombre = nombre,
+    this.puntaje = puntaje,
+    this.opinion = opinion
+  }
+  
+}
+
+const arrayOpinion = JSON.parse( localStorage.getItem("array")) || [];
+
+window.addEventListener("load", () => {
+  if (arrayOpinion.length > 0) {
+    generarInterfaz(arrayOpinion)
+  }
+})
+let bandera = false
+let form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  let nodo = e.target.children;
+
+  if(bandera) {
+    console.log("actualizando");
+    editarCampos()
+    bandera = false
+  }else{
+    const Opinion = new Opiniones (nodo[0].value,nodo[1].value,nodo[2].value,nodo[3].value)
+    arrayOpinion.push(Opinion)
+    generarInterfaz(arrayOpinion)
+  }
+  localStorage.setItem("array", JSON.stringify(arrayOpinion))
+  console.log(arrayOpinion);
+  form.reset()
+})
+
+
+const generarInterfaz = (arr) => {
+  let containerOpiniones = document.getElementById("containerOpiniones")
+containerOpiniones.innerHTML = "";
+  arr.map( el => containerOpiniones.innerHTML += `
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6 mt-2">
+        <div class="card id="${el.nombre}">
+        <div class="card-body">
+          <h5 class="card-title">${el.nombre}</h5>
+          <p class="card-text">${el.puntaje}</p>
+          <p class="card-text">${el.opinion}</p>
+          <button type="button" class="btn btn-danger btn_eliminar">Borrar</button>
+          <button type="button" class="btn btn-primary btn_actualizar">Actualizar</button>
+          </div>
+        </div>                
+      </div>
+    </div>
+</div>
+    `
+  )
+
+  eliminar()
+  actualizar()
+}
+
+const eliminar = () => {
+  let btnEliminar = document.querySelectorAll(".btn_eliminar")
+  for (const btn of btnEliminar) {
+    btn.addEventListener("click", (event) => {
+      let nodo = event.path[2]
+      let buscar = arrayOpinion.findIndex( el => el.nombre == nodo.id);
+      arrayOpinion.splice(buscar, 1)
+      console.log(arrayOpinion);
+      nodo.remove()
+    })
+  }
+}
+
+const actualizar = () => {
+  let btnActualizar = document.querySelectorAll(".btn_actualizar");
+  for (const btn of btnActualizar) {
+    btn.addEventListener("click", (e) => {
+        bandera = true;
+        let nodo = e.path[2];
+        console.log(e.path);
+        let buscar = arrayOpinion.find( el => el.nombre == nodo.id);
+        console.log(nodo);
+        document.getElementById("nombre").value = buscar.nombre;
+        document.getElementById("puntaje").value = buscar.puntaje;
+        document.getElementById("opinion").value = buscar.opinion;
+    })
+  }
+
+}
+
+const editarCampos = () => {
+  let id = document.getElementById("nombre").value 
+  console.log(id);
+  let buscar = arrayOpinion.findIndex(el => el.nombre == id)
+  console.log(arrayOpinion);
+  console.log(buscar);
+  console.log( arrayOpinion[buscar] );
+  arrayOpinion[buscar].puntaje =  document.getElementById("puntaje").value
+  arrayOpinion[buscar].opinion =  document.getElementById("opinion").value
+  generarInterfaz(arrayOpinion)
+}
+
+let input_search = document.getElementById("input_search");
+
+
